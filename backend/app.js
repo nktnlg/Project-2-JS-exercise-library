151db6b8b4718a90ea743488edad42308a28d771
nktnlg <2eleven94@gmail.com>
2022-08-12 18:17:58 +0300

    basic backend server code

---
+++ backend/app.js
@@ -0,0 +1,10 @@
+const colors = require('colors')
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.status(200).json({message: 'working'})
+    })
+
+module.exports = app;
