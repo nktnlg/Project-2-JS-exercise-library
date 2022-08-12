34c828c26a03cbb4747bfa3a04079e903ffdfc34
nktnlg <2eleven94@gmail.com>
2022-08-12 18:45:29 +0300

    fixed gitignore problem

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,9 @@ const colors = require('colors')
 const express = require('express');
 const app = express();
 
+
+const morgan = require('morgan')
+
 app.get('/', (req, res)=>{
     res.status(200).json({message: 'working'})
     })
